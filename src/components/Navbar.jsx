bba1e7878fe56280005391ced51a5796ef94a476
stormchin <38873279+stormchin@users.noreply.github.com>
2025-06-16 23:54:58 +0000

    jamaica navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
         fontSize: '1.6rem',
         textDecoration: 'none',
         letterSpacing: '0.1em'
-      }}>Storm & Lucas: Jamaica 2025</Link>
+      }}>Jamaica '25</Link>
       <div>
         <Link to="/portland" style={{
           color: location.pathname === '/portland' ? '#00e676' : '#fff',
